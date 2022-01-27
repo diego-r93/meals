5c0928910269d1b9467df14252864e7917db4363
diego <diegorodrigues-5@poli.ufrj.br>
2022-01-27 17:09:07 -0300

    :tada: Criando estrutura basica do App

---
+++ lib/components/category_item.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../models/category.dart';
+import '../utils/app_routes.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem(this.category, {Key? key}) : super(key: key);
+
+  final Category category;
+
+  void _selectedCategory(BuildContext context) {
+    Navigator.of(context).pushNamed(
+      AppRoutes.CATEGORIES_MEALS,
+      arguments: category,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => _selectedCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/categories_meals_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../components/meal_item.dart';
+import '../models/category.dart';
+import '../data/dummy_data.dart';
+
+class CategoriesMealsScreen extends StatelessWidget {
+  const CategoriesMealsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final category = ModalRoute.of(context)!.settings.arguments as Category;
+
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(category.title),
+      ),
+      body: Center(
+        child: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: (ctx, index) {
+            return MealItem(categoryMeals[index]);
+          },
+        ),
+      ),
+    );
+  }
+}
