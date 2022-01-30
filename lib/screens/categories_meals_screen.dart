ce808799270a7ce60720dd6a8faa4c7eb9d514d7
diego <diegorodrigues-5@poli.ufrj.br>
2022-01-30 13:43:40 -0300

    :zap: Configurando favoritos

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 
 import '../components/meal_item.dart';
 import '../models/category.dart';
-import '../data/dummy_data.dart';
+import '../models/meal.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-  const CategoriesMealsScreen({Key? key}) : super(key: key);
+  const CategoriesMealsScreen(this.meals, {Key? key}) : super(key: key);
+
+  final List<Meal> meals;
 
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)!.settings.arguments as Category;
 
-    final categoryMeals = DUMMY_MEALS.where((meal) {
+    final categoryMeals = meals.where((meal) {
       return meal.categories.contains(category.id);
     }).toList();
 
